da6d4d9ca0b13b340e61d776aa060e316d3f6e08
github username <github email address>
2019-02-19 02:34:40 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,27 +2,15 @@ class Pirate
 	attr_accessor :name, :weight, :height
 	@@all = []
 
-<<<<<<< HEAD
-	def initialize(args)
-	  @name = args[:name]
-	  @weight = args[:weight]
-	  @height = args[:height]
-	  @@all << self
-=======
 	def initialize(params)
 		@name = params[:name]
 		@weight = params[:weight]
 		@height = params[:height]
 		@@all << self
->>>>>>> 790b749c224fa9ede8b789d5b3d57854bb3910a4
 	end
 
 	def self.all
 		@@all
 	end
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 790b749c224fa9ede8b789d5b3d57854bb3910a4
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,17 +2,11 @@ class Ship
 	attr_accessor :name, :type, :booty
 	@@all = []
 
-<<<<<<< HEAD
-	def initialize(args)
-		@name = args[:name]
-		@type = args[:type]
-		@booty = args[:booty]
-=======
+
 	def initialize(ship_hash)
 		@name = ship_hash[:name]
 		@type = ship_hash[:type]
 		@booty = ship_hash[:booty]
->>>>>>> 790b749c224fa9ede8b789d5b3d57854bb3910a4
 		@@all << self
 	end
 
@@ -23,8 +17,5 @@ class Ship
 	def self.clear
 		@@all = []
 	end
-<<<<<<< HEAD
-=======
 
->>>>>>> 790b749c224fa9ede8b789d5b3d57854bb3910a4
 end
